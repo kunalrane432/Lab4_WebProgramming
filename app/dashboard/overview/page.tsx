6e4ede08a988832c79cc306cd7266a57b0728802
Kunal Rane <kunalrane432@gmail.com>
2024-06-04 10:39:22 -0400

    chapters 5-10

--- app/dashboard/overview/page.tsx
+++ app/dashboard/overview/page.tsx
@@ -5,8 +5,8 @@ import { lusitana } from '@/app/ui/fonts';
 //import { fetchRevenue } from '@/app/lib/data';
 import { fetchCardData } from '@/app/lib/data'; 
 import { Suspense } from 'react';
-import { RevenueChartSkeleton ,LatestInvoicesSkeleton,CardsSkeleton} from '@/app/ui/skeletons'; 
-import CardWrapper from '@/app/ui/dashboard/cards';
+import { RevenueChartSkeleton ,LatestInvoicesSkeleton} from '@/app/ui/skeletons'; 
+
 
 
 export default async function Page() {
@@ -34,9 +34,6 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-      <Suspense fallback={<CardsSkeleton />}>
-          <CardWrapper />
-        </Suspense>
       <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
